6e3e9d8d96118fcf17cfc374393ac66f7f175bbf
AKA-SSH <akashsingh99.as99@gmail.com>
2024-02-08 13:19:18 +0530

    evaluate model updated

--- utils/evaluate_model.py
+++ utils/evaluate_model.py
@@ -1,30 +1,23 @@
-import numpy as np
-import pandas as pd
-from sklearn.metrics import classification_report
 from tabulate import tabulate
+from sklearn.metrics import r2_score, mean_absolute_error, root_mean_squared_error
 
 def evaluate_model(true, pred):
     '''
-    This function returns a classification report for accuracy, precision, recall, and F1 score.
+    This function returns an evaluation table with accuracy, precision, recall and F1 score
 
     Input:
         - True Value
         - Predicted Value
     
     Output:
-        - Classification Report
+        - Evaluation Table
     '''
-    try:
-        true_values = true.values.ravel() if isinstance(true, pd.Series) or isinstance(true, pd.DataFrame) else true
-        pred_values = pred.ravel() if isinstance(pred, np.ndarray) else pred.values.ravel()
-
-        true_int = true_values.astype(int)
-        pred_int = pred_values.astype(int)
-
-        class_report = classification_report(true_int, pred_int, target_names=['Class 0', 'Class 1'], output_dict=True)
-        class_report_table = tabulate(pd.DataFrame.from_dict(class_report), headers='keys', tablefmt='grid')
-
-        return f"Classification Report:\n{class_report_table}"
-
-    except Exception as e:
-        return f"Error during evaluation: {str(e)}"
+    MAE= f'{mean_absolute_error(true, pred):.3f}'
+    RMSE= f'{root_mean_squared_error(true, pred):.3f}'
+    R2= f'{r2_score(true, pred):.3f}'
+    table= [['RMSE', RMSE],
+            ['MAE', MAE],
+            ['R2', R2]]
+    
+    evaluation= tabulate(table, headers= ['METRIC', 'SCORE'], tablefmt= 'grid')
+    return evaluation
